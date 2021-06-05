eadbe0cafc840a6b7331d4b4745384d8cbb7410d
Manish Pandey <manishpandey073536@gmail.com>
2021-06-05 14:58:34 +0545

    second

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("smNav").style.width = "150px";
+    document.getElementById("main").style.marginLeft="100px";
+
+}
+
+function closeNav(){
+    document.getElementById("smNav").style.width = "0";
+    document.getElementById("main").style.width = "0";
+}
